060cfcf71f5519387a6635660b8b3caa2403fd72
Cory Pavitt <corypavitt@hotmail.com>
2018-04-25 17:05:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo (holidayDay, name) {
   return(`Happy ${holidayDay}, ${name}!`)
 }
+
+function holidayCountdown(holidayDay, daysleft) {
+  return(`It's ${daysleft} days until ${holidayDay}!`)
+}
